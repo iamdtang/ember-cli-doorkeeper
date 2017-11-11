db3d583ab6bc5e5c4848cb89fba888b9989a7e73
David Tang <dtang859@gmail.com>
2017-11-10 16:18:47 -0800

    console formatting

--- src/display-coverage.js
+++ src/display-coverage.js
@@ -4,9 +4,9 @@ module.exports = function displayCoverage(coverage, title) {
   this.ui.writeLine('');
   this.ui.writeLine(title);
   this.ui.writeLine('================');
-  this.ui.writeLine(`Lines: ${coverage.lines}`);
-  this.ui.writeLine(`Statements: ${coverage.statements}`);
-  this.ui.writeLine(`Functions: ${coverage.functions}`);
-  this.ui.writeLine(`Branches: ${coverage.branches}`);
+  this.ui.writeLine(`Lines: ${coverage.lines}%`);
+  this.ui.writeLine(`Statements: ${coverage.statements}%`);
+  this.ui.writeLine(`Functions: ${coverage.functions}%`);
+  this.ui.writeLine(`Branches: ${coverage.branches}%`);
   this.ui.writeLine('');
 }
