de3aa43e00b414dadf276386e14974ee201e6072
David Tang <dtang859@gmail.com>
2017-11-26 13:27:27 -0800

    only commit coverage/coverage-summary.json

--- src/commit-coverage-results.js
+++ src/commit-coverage-results.js
@@ -3,7 +3,7 @@
 const shell = require('shelljs');
 
 module.exports = function commitCoverageResults() {
-  shell.exec('git add coverage/coverage-summary.json');
+  shell.exec('git add ./coverage/coverage-summary.json');
   shell.exec('git commit -m "Auto commit coverage/coverage-summary.json"');
   shell.exec('git config --global push.default current');
   shell.exec('git push');
